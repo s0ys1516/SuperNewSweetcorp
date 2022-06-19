f3b3f0d2255874651d97e52fd8728b071d88ec82
s0ys1516 <klevtsevitch.lev@yandex.ru>
2022-06-19 11:50:14 +0300

    Ссылки на форму, фикс корзин у карточек продуктов

--- source/js/main.js
+++ source/js/main.js
@@ -1,10 +1,10 @@
-import {ieFix} from './utils/ie-fix';
-import {iosVhFix} from './utils/ios-vh-fix';
+import { ieFix } from './utils/ie-fix';
+import { iosVhFix } from './utils/ios-vh-fix';
 
-import {initModals} from './modules/init-modals';
+import { initModals } from './modules/init-modals';
 
 //-- этот скрипт загружает модуль для тестирования, его не должно быть в финальном билде и на проде для клиента
-import {testInstruments} from './vendor/testInstruments';
+import { testInstruments } from './vendor/testInstruments';
 //--
 
 // Utils
@@ -23,3 +23,19 @@ window.addEventListener('load', () => {
   testInstruments();
 });
 //--
+
+// Плавная прокрутка
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+
+    const blockID = anchor.getAttribute('href').substr(1)
+
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
+}
